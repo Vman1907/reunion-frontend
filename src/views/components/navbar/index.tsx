142612a121b118c9e9e786a68d73ee5144092e84
varshmaan-yaro <124766721+varshmaan-yaro@users.noreply.github.com>
2023-09-28 01:15:07 +0530

    filter popover

--- src/views/components/navbar/index.tsx
+++ src/views/components/navbar/index.tsx
@@ -1,6 +1,11 @@
-import { Button, Flex, HStack, Text } from "@chakra-ui/react";
+import { Button, Flex, HStack, Image, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { LOGO } from "../../../assets/images";
+import { ROUTES } from "../../../config/const";
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
     return (
         <HStack
             width={"full"}
@@ -9,14 +14,28 @@ const Navbar = () => {
             px={"2rem"}
             backgroundColor={"#eee"}
         >
-            <Flex>
+            <Flex alignItems={"center"} gap={"4"}>
+                <Image
+                    src={LOGO}
+                    alt=""
+                    height={"50px"}
+                    mixBlendMode={"color-burn"}
+                />
                 <Text>Home</Text>
             </Flex>
             <Flex gap={"2rem"}>
-                <Button colorScheme="facebook" size={"sm"}>
+                <Button
+                    colorScheme="blue"
+                    size={"sm"}
+                    onClick={() => navigate(ROUTES.LOGIN)}
+                >
                     Login
                 </Button>
-                <Button colorScheme="facebook" size={"sm"}>
+                <Button
+                    colorScheme="cyan"
+                    size={"sm"}
+                    onClick={() => navigate(ROUTES.SIGNUP)}
+                >
                     Signup
                 </Button>
             </Flex>
--- src/views/pages/home/components/filter.tsx
+++ src/views/pages/home/components/filter.tsx
@@ -1,65 +1,282 @@
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Flex,
+    Popover,
+    PopoverArrow,
+    PopoverBody,
+    PopoverCloseButton,
+    PopoverContent,
+    PopoverHeader,
+    PopoverTrigger,
+    RangeSlider,
+    RangeSliderFilledTrack,
+    RangeSliderThumb,
+    RangeSliderTrack,
+    Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const Filter = () => {
+    const [range, setRange] = useState([10, 300]);
+
     return (
         <Flex
             backgroundColor={"#eee"}
             p={"1rem"}
             rounded={"2xl"}
             flexWrap={"wrap"}
-            className="w-full md:w-10/12"
+            className="w-full md:w-10/12 gap-4 md:gap-0"
             justifyContent={"space-around"}
         >
-            <Flex className="w-full md:w-1/2" justifyContent={"space-evenly"}>
-                <Button
-                    textAlign={"left"}
-                    variant={"unstyled"}
-                    minWidth={"130px"}
-                >
-                    <Box>
-                        <Text fontSize={"xs"} color={"gray"}>
-                            City
-                        </Text>
-                        <Text fontSize={"sm"}>Select Location</Text>
-                    </Box>
-                </Button>
-                <Button
-                    textAlign={"left"}
-                    variant={"unstyled"}
-                    minWidth={"130px"}
-                >
-                    <Box>
-                        <Text fontSize={"xs"} color={"gray"}>
-                            Available from
-                        </Text>
-                        <Text fontSize={"sm"}>Select Move-in Date</Text>
-                    </Box>
-                </Button>
+            <Flex className="w-full md:w-5/12" justifyContent={"space-evenly"}>
+                <Popover placement="bottom-start" matchWidth>
+                    <PopoverTrigger>
+                        <Button
+                            textAlign={"left"}
+                            variant={"unstyled"}
+                            minWidth={"130px"}
+                        >
+                            <Box>
+                                <Text fontSize={"xs"} color={"gray"}>
+                                    City
+                                </Text>
+                                <Text fontSize={"sm"}>Select Location</Text>
+                            </Box>
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverBody>
+                            <Flex direction={"column"} gap={"2"}>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Mumbai, Maharashtra
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    {" "}
+                                    Delhi, Delhi
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Bangalore, Karnataka
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Hyderabad, Telangana
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Chennai, Tamil Nadu
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Kolkata, West Bengal
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Pune, Maharashtra
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Ahmedabad, Gujarat
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Jaipur, Rajasthan
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Surat, Gujarat
+                                </Button>
+                            </Flex>
+                        </PopoverBody>
+                    </PopoverContent>
+                </Popover>
+                <Popover>
+                    <PopoverTrigger>
+                        <Button
+                            textAlign={"left"}
+                            variant={"unstyled"}
+                            minWidth={"130px"}
+                        >
+                            <Box>
+                                <Text fontSize={"xs"} color={"gray"}>
+                                    Available from
+                                </Text>
+                                <Text fontSize={"sm"}>Select Move-in Date</Text>
+                            </Box>
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>Confirmation!</PopoverHeader>
+                        <PopoverBody></PopoverBody>
+                    </PopoverContent>
+                </Popover>
             </Flex>
-            <Flex className="w-full md:w-1/2" justifyContent={"space-evenly"}>
-                <Button
-                    textAlign={"left"}
-                    variant={"unstyled"}
-                    minWidth={"130px"}
-                >
-                    <Box>
-                        <Text fontSize={"xs"} color={"gray"}>
-                            Price
-                        </Text>
-                        <Text fontSize={"sm"}>Select Price Range</Text>
-                    </Box>
-                </Button>
-                <Button
-                    textAlign={"left"}
-                    variant={"unstyled"}
-                    minWidth={"130px"}
-                >
-                    <Box>
-                        <Text fontSize={"xs"} color={"gray"}>
-                            Property Type
-                        </Text>
-                        <Text fontSize={"sm"}>Select Property Type</Text>
-                    </Box>
+            <Flex className="w-full md:w-5/12" justifyContent={"space-evenly"}>
+                <Popover>
+                    <PopoverTrigger>
+                        <Button
+                            textAlign={"left"}
+                            variant={"unstyled"}
+                            minWidth={"130px"}
+                        >
+                            <Box>
+                                <Text fontSize={"xs"} color={"gray"}>
+                                    Price
+                                </Text>
+                                <Text fontSize={"sm"}>Select Price Range</Text>
+                            </Box>
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>Confirmation!</PopoverHeader>
+                        <PopoverBody>
+                            <Text>
+                                {range[0]} {range[1]}
+                            </Text>
+                            <RangeSlider
+                                aria-label={["min", "max"]}
+                                defaultValue={[10, 30]}
+                                value={range}
+                                onChange={(value) => setRange(value)}
+                            >
+                                <RangeSliderTrack>
+                                    <RangeSliderFilledTrack />
+                                </RangeSliderTrack>
+                                <RangeSliderThumb index={0} />
+                                <RangeSliderThumb index={1} />
+                            </RangeSlider>
+                        </PopoverBody>
+                    </PopoverContent>
+                </Popover>
+                <Popover>
+                    <PopoverTrigger>
+                        <Button
+                            textAlign={"left"}
+                            variant={"unstyled"}
+                            minWidth={"130px"}
+                        >
+                            <Box>
+                                <Text fontSize={"xs"} color={"gray"}>
+                                    Property Type
+                                </Text>
+                                <Text fontSize={"sm"}>
+                                    Select Property Type
+                                </Text>
+                            </Box>
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>Confirmation!</PopoverHeader>
+                        <PopoverBody>
+                            <Flex direction={"column"} gap={"2"}>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Apartment
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    House
+                                </Button>
+                                <Button
+                                    textAlign={"left"}
+                                    variant={"unstyled"}
+                                    fontSize={"sm"}
+                                    width={"200px"}
+                                    _hover={{ backgroundColor: "#eee" }}
+                                    pl={"1rem"}
+                                >
+                                    Vacation rental
+                                </Button>
+                            </Flex>
+                        </PopoverBody>
+                    </PopoverContent>
+                </Popover>
+            </Flex>
+            <Flex className="w-full md:w-2/12">
+                <Button width={"full"} size={"sm"}>
+                    Apply
                 </Button>
             </Flex>
         </Flex>
